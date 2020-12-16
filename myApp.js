774d97619fc243f4782b4c1bccc83f72104b74c3
Zaki <zakiabdulle1@gmail.com>
2020-12-16 17:31:19 +0000

    copying previous challenge from https://repl.it/@zaki501/boilerplate-express-1#myApp.js

--- myApp.js
+++ myApp.js
@@ -2,6 +2,10 @@ var express = require('express');
 var app = express();
 
 
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/views/index.html")
+});
+app.use(express.static(__dirname + "/public"));
 
 
 
