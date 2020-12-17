4a91885d5ac79d8dde8f49ff85ff1f8b53ac1d28
Zaki <zakiabdulle1@gmail.com>
2020-12-17 16:43:32 +0000

    changing / path to /Json

--- myApp.js
+++ myApp.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var app = express();
 
-/*
+
 app.get("/", function(req, res){
   res.sendFile(__dirname + "/views/index.html")
 });
-*/
+
 app.use(express.static(__dirname + "/public"));
 
-app.get("/", function(req, res){
+app.get("/json", function(req, res){
   res.json({"message": "Hello json"})
 })
 
