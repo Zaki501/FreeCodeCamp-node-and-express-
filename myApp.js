958ed37f4f396c4e5d1fb2e4731a888f3be66eb7
Zaki <zakiabdulle1@gmail.com>
2020-12-17 13:46:48 +0000

    sending JSON task

--- myApp.js
+++ myApp.js
@@ -1,12 +1,16 @@
 var express = require('express');
 var app = express();
 
-
+/*
 app.get("/", function(req, res){
   res.sendFile(__dirname + "/views/index.html")
 });
+*/
 app.use(express.static(__dirname + "/public"));
 
+app.get("/", function(req, res){
+  res.json({"message": "Hello json"})
+})
 
 
 
